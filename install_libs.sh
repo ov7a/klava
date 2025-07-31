3528258a03591892a6d6c28c2e18d0c0b4b107b5
ov7a <ov7a@yandex.ru>
2025-07-31 19:18:00 +0200

    simplify libs inclusion via -i./libs

--- install_libs.sh
+++ install_libs.sh
@@ -22,6 +22,4 @@ function install {
 mkdir -p "libs"
 cd "libs"
 install "https://github.com/koka-community/std" "std" "b5ff42dda3976a9840a1aad2a731dbdc8bd6e7d7"
-rm -rf ../std
-mv std .. # because it has self-references. and to move it upper in order of includes
 install "https://github.com/koka-community/html" "html" "7c97be90e88db69e313c3a19794a5dc786b4b828"
