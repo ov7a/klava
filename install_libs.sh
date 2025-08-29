7ce407603a0df1075d9664db9f1ab97b7c50c1f7
ov7a <ov7a@yandex.ru>
2025-08-30 00:00:18 +0200

    add testing infra

--- install_libs.sh
+++ install_libs.sh
@@ -21,5 +21,5 @@ function install {
 
 mkdir -p "libs"
 cd "libs"
-install "https://github.com/koka-community/std" "std" "b5ff42dda3976a9840a1aad2a731dbdc8bd6e7d7"
+install "https://github.com/koka-community/std" "std" "6398eb3af8c7d50c24573f391a4dede45cd342a6"
 install "https://github.com/koka-community/html" "html" "7c97be90e88db69e313c3a19794a5dc786b4b828"
